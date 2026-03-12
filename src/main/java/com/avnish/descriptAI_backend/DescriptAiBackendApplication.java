1bbb59147695a49552e2f40b3a599e81037e4324
AVNISH KUMAR <avnishkrsharma3@gmail.com>
2026-03-12 23:21:05 +0530

    Initial commit - spring boot descriptai-backend project

---
+++ src/main/java/com/avnish/descriptAI_backend/DescriptAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.avnish.descriptAI_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DescriptAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DescriptAiBackendApplication.class, args);
+	}
+
+}
