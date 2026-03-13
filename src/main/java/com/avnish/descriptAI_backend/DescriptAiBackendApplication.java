83088b2ef7d9c88968030bd74174248643a95048
AVNISH KUMAR <avnishkrsharma3@gmail.com>
2026-03-14 05:20:41 +0530

    db connected

--- src/main/java/com/avnish/descriptAI_backend/DescriptAiBackendApplication.java
+++ src/main/java/com/avnish/descriptAI_backend/DescriptAiBackendApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DescriptAiBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DescriptAiBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(DescriptAiBackendApplication.class, args);
+
+    }
 
 }
